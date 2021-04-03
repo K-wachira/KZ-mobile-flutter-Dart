537829bfb4cd71ff0cfc456719036b5e48cd803a
k-wachira <k.wachira@alustudent.com>
2021-04-03 09:52:46 +0300

    removed the non functional homepage

--- alu_express/lib/services/Models/services.dart
+++ alu_express/lib/services/Models/services.dart
@@ -1,8 +1,9 @@
 import 'package:alu_express/services/Models/vendor_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 
-class FirebaseServices {
+class FirebaseServices extends ChangeNotifier {
   FirebaseFirestore _fireStoreDataBase = FirebaseFirestore.instance;
 
   Stream<List<VendorModel>> getFoodList(vendorid) {
