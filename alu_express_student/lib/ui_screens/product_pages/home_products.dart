a97a7ecce64e15eb490f2bc870ccfcf62987a515
Audrey-Njenga <a.njenga@alustudent.com>
2021-04-07 20:03:43 +0200

    Product pop-up

--- alu_express_student/lib/ui_screens/product_pages/home_products.dart
+++ alu_express_student/lib/ui_screens/product_pages/home_products.dart
@@ -1,11 +1,13 @@
 import 'package:alu_express_student/services/Models/firebase_services.dart';
 import 'package:alu_express_student/services/Models/food_model.dart';
 import 'package:alu_express_student/ui_screens/product_pages/product_card.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:alu_express_student/ui_screens/shared_widgets/size_helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:alu_express_student/services/Models/cart_funtionality.dart';
 
 class HomeProducts extends StatefulWidget {
   final userid;
@@ -18,7 +20,155 @@ class _HomeProductsState extends State<HomeProducts> {
   final FirebaseServices firebaseServices = FirebaseServices();
 
   @override
+  int _quantity = 1;
+  final dateTime = DateTime.now();
+  final List cart = [];
+  int total = 0;
+  final CartFunctionality cartFunctionality = CartFunctionality();
+
   Widget build(BuildContext context) {
+    void showProductDetails(Map productDetails) {
+      int price = int.parse(productDetails["Price"]);
+      total = price;
+      void _incrementOrder() {
+        setState(() {
+          _quantity++;
+          total = price * _quantity;
+          print(total);
+        });
+      }
+
+      void _decrementOrder() {
+        setState(() {
+          if (_quantity > 1) {
+            _quantity--;
+            total = price * _quantity;
+            print(total);
+          }
+        });
+      }
+
+      void createitem(cart, totals, quantity, product) {
+        setState(() {
+          totals = totals * quantity;
+        });
+        Map item = {
+          "FoodName": product["Name"],
+          "Quantity": quantity.toString(),
+          "Total": totals.toString(),
+          "Vendor": product["Vendor"],
+          "Customer": product["Userid"],
+          "OrderTime": dateTime,
+          "FoodID": product["FoodID"],
+          'Category': product['Category'],
+          "OrderStatus": "Pending",
+        };
+        cartFunctionality.addToCart(item);
+      }
+
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: productDetails["productName"],
+              actions: <Widget>[
+                // usually buttons at the bottom of the dialog
+                new FlatButton(
+                  child: new Text("Close"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                new FlatButton(
+                  child: new Text("Add to Cart"),
+                  onPressed: () {},
+                )
+              ],
+              content: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    productDetails["Name"],
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: .3),
+                  ),
+                  Text(
+                    productDetails["Description"],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: .3),
+                  ),
+                  Image(
+                    image: NetworkImage(productDetails["Image"]),
+                    height: displayHeight(context) * 0.2,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Center(
+                      child: Text(
+                        (total * _quantity).toString(),
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: .3),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _incrementOrder();
+                            });
+                          },
+                          icon: Icon(Feather.plus),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Text(
+                            '$_quantity',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: .3),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: IconButton(
+                            icon: Icon(Feather.minus),
+                            onPressed: () {
+                              setState(() {
+                                _decrementOrder();
+                              });
+                            }),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            );
+          });
+    }
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -74,7 +224,7 @@ class _HomeProductsState extends State<HomeProducts> {
                 child: StreamProvider(
                   create: (BuildContext context) =>
                       firebaseServices.getFoodList(),
-                  child: ViewUserPage(),
+                  child: ViewUserPage(showProductDetails),
                 ),
               )
             ],
@@ -84,12 +234,17 @@ class _HomeProductsState extends State<HomeProducts> {
 }
 
 class ViewUserPage extends StatelessWidget {
+  ViewUserPage(this.showDialog);
+  final Function showDialog;
   @override
   Widget build(BuildContext context) {
     List userList = Provider.of<List<FoodModel>>(context);
 
     return userList == null
-        ? CircularProgressIndicator()
+        ? Padding(
+            padding: const EdgeInsets.all(100.0),
+            child: CircularProgressIndicator(),
+          )
         : GridView.builder(
             gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                 maxCrossAxisExtent: 300,
@@ -100,6 +255,7 @@ class ViewUserPage extends StatelessWidget {
               return Container(
                 alignment: Alignment.center,
                 child: ProductCard(
+                  showDialog: showDialog,
                   image: userList[index].imageURL,
                   name: userList[index].foodName,
                   price: userList[index].price,
@@ -117,5 +273,109 @@ class ViewUserPage extends StatelessWidget {
                 ),
               );
             });
+  }
+}
+
+class ProductCard extends StatefulWidget {
+  final Function showDialog;
+  final String image;
+  final String name;
+  final String price;
+  final String category;
+  final String description;
+  final String discount;
+  final String isFeaured;
+  final String size;
+  final String ingredients;
+  final String vendor;
+  final String foodid;
+  final String timeuploaded;
+  final String quantity;
+  final int indexs;
+
+  const ProductCard(
+      {Key key,
+      this.image,
+      this.name,
+      this.price,
+      this.category,
+      this.description,
+      this.discount,
+      this.isFeaured,
+      this.size,
+      this.ingredients,
+      this.vendor,
+      this.foodid,
+      this.timeuploaded,
+      this.quantity,
+      this.indexs,
+      this.showDialog})
+      : super(key: key);
+
+  @override
+  _ProductCardState createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  @override
+  Widget build(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+
+    return GestureDetector(
+      onTap: () {
+        Map productDetails = {
+          "Image": widget.image,
+          "Name": widget.name,
+          "Price": widget.price,
+          "Description": widget.description,
+          "Ingredients": widget.ingredients,
+          "Vendor": widget.vendor,
+          "Userid": auth.currentUser.uid,
+          "Category": widget.category,
+          "Discount": widget.discount,
+          "Size": widget.size,
+          "FoodID": widget.foodid,
+          "timeuploaded": widget.timeuploaded,
+          'index': widget.indexs
+        };
+        widget.showDialog(productDetails);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(padding: EdgeInsets.only(top: 5)),
+          Image(
+            width: displayWidth(context) * 0.4,
+            image: NetworkImage(widget.image),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(30)),
+            width: displayWidth(context) * 0.4,
+            padding: EdgeInsets.all(5),
+            child: Column(
+              children: [
+                Text(
+                  widget.name,
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.red[900],
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'RWF ' + widget.price,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400),
+                ),
+                // Text(widget.description)
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
